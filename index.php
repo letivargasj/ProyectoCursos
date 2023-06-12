5a165f2964584dad3abc437b123cf6573339e84d
Leticia Vargas Jaramillo <73510806+letivargasj@users.noreply.github.com>
2023-06-11 21:56:51 -0600

    String y reset icluido

--- index.php
+++ index.php
@@ -9,24 +9,27 @@
     $nombre = $_POST['buscar'];
 
     if(!empty($_POST["btnFiltro"])) {
-
-        if ((!empty($_POST['filtroCategoria'])) && (!empty($_POST['buscar']))){
-            $sql  = "SELECT * FROM curso WHERE cur_categoria = '$categoria' AND cur_nombre = '$nombre'";
+        if ((empty($_POST['filtroCategoria'])) && (empty($_POST['buscar']))){
+            $sql  = "SELECT * FROM curso";
             $query = mysqli_query($conn, $sql);
 
         }else if((empty($_POST['filtroCategoria'])) && (!empty($_POST['buscar']))){    
-            $sql  = "SELECT * FROM curso WHERE cur_nombre = '$nombre'";
+            $sql  = "SELECT * FROM curso WHERE cur_nombre LIKE '%$nombre%'";
             $query = mysqli_query($conn, $sql);
 
         }else if ((!empty($_POST['filtroCategoria'])) && (empty($_POST['buscar']))){
             $sql  = "SELECT * FROM curso WHERE cur_categoria = '$categoria'";
             $query = mysqli_query($conn, $sql);
             
-        } else { 
-            $sql  = "SELECT * FROM curso";
-            $query = mysqli_query($conn, $sql);
-
-        }
+        }else if((!empty($_POST['filtroCategoria'])) && (!empty($_POST['buscar']))){
+            $sql  = "SELECT * FROM curso WHERE cur_categoria = '$categoria' AND cur_nombre LIKE '%$nombre%'";
+            $query = mysqli_query($conn, $sql); 
+        } 
+    }
+    
+    if(!empty($_POST["btnReset"])) {
+        $sql  = "SELECT * FROM curso";
+        $query = mysqli_query($conn, $sql);
     }
 ?>
 
@@ -41,8 +44,7 @@
         <link rel="stylesheet" href="/css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        
-        <title>ENSIGNA</title>
+         <title>ENSIGNA</title>
     </head>
     <body>
         <div class="principal">
@@ -82,11 +84,15 @@
                     <input 
                     type="text" 
                     name="buscar" 
-                    placeholder="Escribe el nombre de la materia"
+                    placeholder="Busca nombre del curso"
                     class="inputBuscar"
                     id="inputBuscar"
-                    >
-                    <input type="submit" name="btnFiltro" class="btn btn-dark" id="btnFiltrar" value="Buscar">
+                    >  
+                    <input type="submit" name="btnFiltro" class="btn btn-dark mr-3" id="btnFiltrar" value="Buscar">
+
+                    <input type="submit" name="btnReset" class="btn btn-white " id="btnReset" value="">
+                    <label for="btnReset"><i class="fa-solid fa-rotate-left"></i></label>
+
                 </form>
             </div>
                 
@@ -123,6 +129,7 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/e8f8d6036e.js" crossorigin="anonymous"></script>
     </body>
 
 </html>
