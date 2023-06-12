6dce7d7efc0f6f062d6efd0cbab29dd2307be534
anhelikscbr1 <anhelik97@gmail.com>
2023-06-12 08:30:50 -0600

    cambios_finales

--- controlador/controlador_inscribirse.php
+++ controlador/controlador_inscribirse.php
@@ -1,31 +1,30 @@
 <?php       
+    #include("conexion.php");
+    mysqli_close($mysqli);
     session_start();
-
-    $id = $_GET['id'];
-    echo $id;
-    $usuario = $_SESSION["usuario"]; 
-    echo $usuario;
-    
     if(empty($_SESSION["usuario"])){
         header("location: ../login.php");
     }
     if($_SESSION["tipo"] == "mae"){
         header("location: ../index.php");
     }
+    #$conn = conectar();
+    #$id = $_GET['id'];
+    #echo $id;
+    #$usuario = $_SESSION["usuario"]; 
+    #echo $usuario;
+    #$sql  = "SELECT * FROM curso";
 
-    $conn = conectar();
-    $sql  = "INSERT INTO detalle (det_cur_id, det_est_id) 
-            VALUES('$id', '$usuario')";
-    
-    $query = mysqli_query($conn, $sql);
-    if($query) {
-        header("Location: ../miscursos.php");
-        echo $query;
-        echo "<script>alert('yyyyy');</script>";
-    } else {
-        echo $query;
-        echo "<script>alert('bbbbbbbbbb');</script>";
-    }
+    #$sql  = "INSERT INTO detalle(det_cur_id, det_est_id) VALUES('$id', '$usuario')";
+    #$query2 = mysqli_query($conn, $sql);
+    header("Location: ../miscursos.php");
+#    if($query) {
+        #header("Location: ../miscursos.php");
+
+    #} else {
+        #header("Location: ../miscursos.php");
+
+    #}
 
 
 ?>
